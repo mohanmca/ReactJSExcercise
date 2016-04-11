86a2ea8a8a6851b095fa5d9caf4eca3463cce7f1
mohanmca <projectnash1994@Gmail.com>
2016-04-12 04:51:43 +0800

    Import draft

---
+++ gitscript.sh
@@ -0,0 +1,6 @@
+echo "# ReactJSExcercise" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mohanmca/ReactJSExcercise.git
+git push -u origin master
