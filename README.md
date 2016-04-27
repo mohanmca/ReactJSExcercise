0245a6fcdd90034521b5097a7cb617d0e485f3c5
unknown <mohanmca@gmail.com>
2016-04-27 15:50:42 +0800

    React component

--- README.md
+++ README.md
@@ -28,3 +28,27 @@ https://github.com/reactjs/react-router-tutorial/tree/master/lessons
 Warren resources
 ~~~~~~~~~~~~~~~~
 https://github.com/wmira/functional-javascript-resources
+
+
+*   Separating components into container components and presentational components, with presentational components optionally taking in some data and rendering a view.
+*   Stateless Functional Components are that they don't support shouldComponentUpdate
+*   PropTypes in React are the middle ground in terms of type checking properties that are passed to your components.- https://facebook.github.io/react/docs/reusable-components.html
+*   PropTypes are great for finding bugs in your components
+*   
+
+  
+PropTypes
+var Icon = React.createClass({
+  propTypes: {
+    name: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
+    color: PropTypes.string.isRequired,
+    optionalArray: PropTypes.array.isRequired,
+    optionalBool: PropTypes.bool.isRequired,
+    optionalFunc: PropTypes.func.isRequired,    
+    style: PropTypes.object
+  },
+  render: ...
+});
+
+
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,5 @@
 var React = require("react")
-var transparentBg = require('../styles/index').transparentBg
+var Prompt = require("../components/Prompt")
 
 //var ReactRouter = require('react-router')
 
@@ -7,11 +7,11 @@ var PromptContainer = React.createClass({
 	contextTypes:  {
 		router: React.PropTypes.object.isRequired
 	},
-	onUpdateUser(event) {
+	handleUpdateUser(event) {
 		event.preventDefault();
 		this.setState({username: event.target.value});
 	},
-	onSubmitUser(event) {
+	handleSubmitUser(event) {
 		event.preventDefault();
 		var username = this.state.username;
 		this.setState({username: ''});
@@ -27,30 +27,19 @@ var PromptContainer = React.createClass({
 			this.context.router.push('/playerTwo/'+this.state.username);
 		}
 	},
-	getInitialState : function(){
+	getInitialState : function() {
 		return {
 			username: ''
 		}
 	},
 	 render : function() {
      return (
-       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-         <h1>{this.props.route.header}</h1>
-         <div className="col-sm-12">
-           <form onSubmit={this.onSubmitUser}>
-             <div className="form-group">
-               <input className="form-control" placeholder="Github username"
-								 value={this.state.username}
-								 type="text" onChange={this.onUpdateUser}/>
-             </div>
-             <div className="form-group col-sm-4 col-sm-offset-4">
-               <button className="btn btn-block btn-succcess" type="Submit" >
-                  Continue
-               </button>
-             </div>
-           </form>
-       </div>
-     </div>
+    		<Prompt
+					onSubmitUser={this.handleSubmitUser}
+					onUpdateUser={this.handleUpdateUser}
+					header={this.props.route.header}
+					username={this.state.username}
+					/>
      );
    }
 })
